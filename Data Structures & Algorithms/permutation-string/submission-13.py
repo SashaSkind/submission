6b2ad66810f34356774b0a8744ef375d10554a68
Sasha <108491618+SashaSkind@users.noreply.github.com>
2026-09-23 17:29:44 -0700

    Add: permutation-string - submission-13

---
+++ Data Structures & Algorithms/permutation-string/submission-13.py
@@ -0,0 +1,37 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        l = 0
+        r = len(s1) - 1
+        dict_of_s1 = defaultdict(int)
+        temp_dict = defaultdict(int)
+
+        for letter in s1:
+            dict_of_s1[letter] += 1
+        for letter in s2[l:r+1]:
+            temp_dict[letter] += 1
+
+        while r < len(s2):
+            while r < len(s2) and dict_of_s1[s2[l]] == 0:
+                l += 1
+                r += 1
+                temp_dict[s2[l-1]] -= 1
+                if r != len(s2):
+                    temp_dict[s2[r]] += 1
+
+            is_same = True
+            for k, v in dict_of_s1.items():
+                if temp_dict[k] != v:
+                    is_same = False
+                    break
+
+            if is_same:
+                return True
+            else:
+                l += 1
+                r += 1
+                if r < len(s2):
+                    temp_dict[s2[l-1]] -= 1
+                    temp_dict[s2[r]] += 1
+
+                
+        return False
